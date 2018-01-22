516912d451dc18c7126c4651e9c111c651835469
MyDickTasteLikeLemons <roman.rumba@hotmail.com>
2018-01-22 21:53:03 +0000

    náði að koma upp upsetningu fyrir EJS template og koma node-sass i gáng

--- vef2-2018-v1/app.js
+++ vef2-2018-v1/app.js
@@ -1,9 +1,40 @@
+/* Requires START */
 const express = require('express');
+const path = require('path');
+/* Requires END */
 
+/* Variables init START */
 const app = express();
 
 const hostname = '127.0.0.1';
 const port = 3000;
+/* Variables init END */
+/* Tengja Css skjal(i/um) */
+app.use(express.static(path.join(__dirname, 'public')));
+/* Tengja views */
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+/* Default fyrir fyrir heimasiðu er byrt index */
+app.get('/', (req, res) => {
+  res.render('index', { title: 'Forsíða' });
+});
+/* Til að henda fólk i villu */
+app.get('/error', (req, res) => {
+  throw new Error('Villa!');
+});
+/* Ef eitthvað er ekki til */
+function notFoundHandler(req, res, next) {
+  res.status(404).send('404 Not Found');
+}
+
+function errorHandler(err, req, res, next) {
+  console.error(err);
+  res.status(500).send('Villa!');
+}
+
+app.use(notFoundHandler);
+app.use(errorHandler);
 
 app.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
